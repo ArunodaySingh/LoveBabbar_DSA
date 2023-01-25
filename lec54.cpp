b5841a20c406fb51f9c0c921c56d9ec2f84840b7
Arunoday <arunodaysingh137@gmail.com>
2023-01-25 23:36:07 +0530

    Changes to be committed: 	new file:   lec54.cpp
    Changes not staged for commit:
    	modified:   Lec4.exe
    	modified:   lec5.cpp
    	modified:   lec5.exe
    	modified:   lec9.cpp
    
    Untracked files:
    	lec44.cpp
    	lec44.exe
    	lec44b.cpp
    	lec44b.exe
    	lec9.exe

---
+++ lec54.cpp
@@ -0,0 +1,28 @@
+// Stack work on LIFO order
+/*
+Insertion (push)
+deletion (pop)
+peek (top)
+Empty (underflow)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+class stack{
+public:
+int top=-1;
+int size;
+//constructor
+stack(int n)
+{
+    size=n;
+    int arr[n];
+}
+
+};
+
+//main function
+int main()
+{
+ return 0;
+}
